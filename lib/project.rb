46a5e0d0b902bdc7ee47adbad53c082c7f7296bd
Amanda Ackerman <ackerm44@gmail.com>
2017-11-02 16:39:00 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,7 +8,9 @@ class Project
 
   def add_backer(backer)
     @backers << backer
-    backer.back_project(self)
+    if (back.back_project(self).nil?)
+      backer.back_project(self)
+    end
   end
 
 end
