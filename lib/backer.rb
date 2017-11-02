96c0f0c0f364e176bc0b91db8d8ac990db95876f
Amanda Ackerman <ackerm44@gmail.com>
2017-11-02 16:17:20 +0000

    automatically backed up by learn

---
+++ lib/backer.rb
@@ -0,0 +1,19 @@
+class Backer
+  attr_accessor :name, :backed_projects, :project
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    @backed_projects << project
+    new_project = Project.new(project)
+    puts self.project
+    #new_project.add_backer(self)
+    #end
+  end
+
+
+
+end
